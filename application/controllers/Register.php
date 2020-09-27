68152276d90c31156846721ad01f6e2901360885
Fatah Noer Arbain <muhammadfatah398@gmail.com>
2020-09-28 01:10:39 +0700

    Menambah fitur Register

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -2,13 +2,178 @@
 
 class Register extends CI_Controller
 {
+   public function __construct()
+   {
+      parent::__construct();
+      $this->load->library('form_validation');
+   }
+
    public function index()
    {
       $data['judul'] = 'Sign Up';
-      $data['css'] = 'signUp.css';
+      $data['css'] = 'signUp2.css';
       $data['script'] = 'signUp.js';
-      $this->load->view('Templates/header', $data);
-      $this->load->view('Register/index');
-      $this->load->view('Templates/footer', $data);
+
+      $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]');
+      $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[users.email]', [
+         'is_unique' => 'Email is already, please another email.'
+      ]);
+      $this->form_validation->set_rules('password', 'Password', 'required|trim|matches[password2]|min_length[4]', [
+         'matches' => 'Password not match!'
+      ]);
+      $this->form_validation->set_rules('password2', 'Confirm Password', 'required|trim|matches[password]|min_length[4]', [
+         'matches' => 'Password not match!'
+      ]);
+
+      if ($this->form_validation->run() === FALSE) {
+         $this->load->view('Templates/header', $data);
+         $this->load->view('Register/index');
+         $this->load->view('Templates/footer', $data);
+      } else {
+         // Insert user to table
+         $username = ucfirst(htmlspecialchars($this->input->post('username', true)));
+         $email = htmlspecialchars($this->input->post('email', true));
+         $password = htmlspecialchars($this->input->post('password', true));
+
+         $data = [
+            'username' => $username,
+            'email' => $email,
+            'password' => password_hash($password, PASSWORD_DEFAULT),
+            'image' => 'default.jpg',
+            'is_active' => 0,
+            'role_id' => 2,
+            'date_create' => time()
+         ];
+
+         // Activated email
+         $token = base64_encode(random_bytes(32));
+         $user_token = [
+            'email' => $email,
+            'token' => $token,
+            'date_create' => time()
+         ];
+
+         $this->db->insert('users', $data);
+         $this->db->insert('user_token', $user_token);
+         $this->_sendEmail($token, 'verify', $email);
+
+         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+         Congratulations! your account has been created, please actived in email.
+       </div>');
+         redirect('Register');
+      }
+   }
+
+   private function _sendEmail($token, $type, $email)
+   {
+      $config = [
+         'protocol' => 'smtp',
+         'smtp_host' => 'ssl://smtp.googlemail.com',
+         'smtp_user' => 'admbikers123@gmail.com',
+         'smtp_pass' => 'akucintakamu',
+         'smtp_port' => 465,
+         'mailtype' => 'html',
+         'charset' => 'utf-8',
+         'newline' => "\r\n",
+      ];
+
+      $message =
+         '<div style="text-align: center; padding: 30px;">
+            <div style="position: relative">
+            <h3 style="text-transform: uppercase;
+                      font-size: 30px;
+                      margin-inline-start: none;
+                      margin-inline-end: none;">
+               Bikes
+            </h3>
+            <p style="position: absolute; top: -10px">Welcome to Bikes Shop!</p>
+            </div>
+            <div style="color: grey;
+                        margin-bottom: 18px;
+                        font-weight: bold;">
+            <p style="margin-bottom: -2px;">Dear : muhammadfatah398@gmail</p>
+            <p>Click active for verification Account!</p>
+            </div>
+            <div>
+               <a href="' . base_url() . 'Register/verify?email=' . $email . '&token=' . urlencode($token) . '" 
+
+               style="width: 100%;
+                      color: white;
+                      background: #1641ff;
+                      border-radius: 20px;
+                      padding: 8px 50px;
+                      text-decoration: none !important;
+                      font-weight: bold;
+                      box-shadow: 0 15px 10px -15px #1641ff;">
+               Active</a>
+            </div>
+         </div>';
+
+      $this->load->library('email');
+      $this->email->initialize($config);
+
+      $this->email->from('admbikers123@gmail.com', 'Admin Bikers');
+      $this->email->to($email);
+
+      if ($type == 'verify') {
+         $this->email->subject('Account Verification');
+         $this->email->message($message);
+      }
+
+      if ($this->email->send()) {
+         return true;
+      } else {
+         echo $this->email->print_debugger();
+         die;
+      }
+   }
+
+   public function verify()
+   {
+      $email = $this->input->get('email');
+      $token = $this->input->get('token');
+
+      $users = $this->db->get('users', ['email' => $email])->row_array();
+
+      // Jika ada user
+      if ($users) {
+         $users_token = $this->db->get('user_token', ['token' => $token])->row_array();
+         // Jika token itu sama
+         if ($users_token) {
+            // Jika token itu lebih dari 1 hari
+            if (time() - $users_token['date_create'] < (60 * 60 * 24)) {
+               $this->db->set('is_active', 1);
+               $this->db->where('email', $email);
+               $this->db->update('users');
+
+               $this->db->delete('user_token', ['email' => $email]);
+
+               $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+               ' . $email . ' has been activated! Please login.</div>');
+               redirect('Register');
+            } else {
+               // Expired
+               $this->db->delete('users', ['email' => $email]);
+               $this->db->delete('user_token', ['email' => $email]);
+
+               $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+               Token expired!.
+             </div>');
+               redirect('Register');
+            }
+         } else {
+            // Jika token itu tidak sama
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Account activated is failed, wrong token.
+          </div>');
+            redirect('Register');
+         }
+      } else {
+         // Jika user itu tidak sama
+         $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+         Account activated is failed, wrong email.
+       </div>');
+         redirect('Register');
+      }
    }
 }
--- application/views/Register/index.php
+++ application/views/Register/index.php
@@ -1,4 +1,3 @@
-<!-- Navbar -->
 <nav class="navbar navbar1 navbar-expand-lg navbar-light fixed-top">
    <div class="container">
       <a class="navbar-brand" href="<?= base_url('Home'); ?>">Bikes</a>
@@ -18,38 +17,137 @@
 
    <div class="row">
       <div class="col-lg-5 col-md-7 mx-auto">
-         <form>
-            <div class="form-group">
-               <label for="Username">Username</label>
-               <input type="text" class="form-control shadow-sm" id="Username" aria-describedby="emailHelp">
-               <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                  else.</small>
-            </div>
-            <div class="form-group">
-               <label for="exampleInputEmail1">Email address</label>
-               <input type="email" class="form-control shadow-sm" id="exampleInputEmail1" aria-describedby="emailHelp">
-               <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                  else.</small>
-            </div>
-            <div class="form-group Password" style="position: relative;">
-               <label for="exampleInputPassword1">Password</label>
-               <input type="password" class="form-control shadow-sm password" id="exampleInputPassword1">
-               <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                  else.</small>
-               <i class="fas fa-eye-slash Show"></i>
-            </div>
 
-            <div class="form-group Password" style="position: relative;">
-               <label for="exampleInputPassword2">Repeat Password</label>
-               <input type="password" class="form-control shadow-sm password2" id="exampleInputPassword2">
-               <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                  else.</small>
+         <?php if ($this->session->flashdata()) : ?>
+            <div class="form-group text-center">
+               <?= $this->session->flashdata('message'); ?>
             </div>
+         <?php endif; ?>
 
-            <div class="form-group">
-               <button type="submit" class="btn btn-danger btn-block shadow-sm">Create Account!</button>
-            </div>
+         <form action="<?= base_url('Register'); ?>" method="POST">
+
+            <?php if (form_error('username')) : ?>
+               <div class="form-group error">
+               <?php else : ?>
+                  <div class="form-group">
+                  <?php endif ?>
+                  <label for="Username">Username</label>
+                  <input type="text" class="form-control shadow-sm" id="Username" aria-describedby="emailHelp" name="username" value="<?= set_value('username'); ?>">
+                  <?= form_error('username', '<small id="emailHelp" class="form-text text-muted">', '</small>'); ?>
+                  <i class="fas fa-exclamation-circle iconError"></i>
+                  </div>
+
+                  <?php if (form_error('email')) : ?>
+                     <div class="form-group error">
+                     <?php else : ?>
+                        <div class="form-group">
+                        <?php endif ?>
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="text" class="form-control shadow-sm" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value="<?= set_value('email'); ?>">
+                        <?= form_error('email', '<small id="emailHelp" class="form-text text-muted">', '</small>'); ?>
+                        <i class="fas fa-exclamation-circle iconError"></i>
+                        </div>
+
+                        <?php if (form_error('password')) : ?>
+                           <div class="form-group error Password" style="position: relative;">
+                           <?php else : ?>
+                              <div class="form-group Password" style="position: relative;">
+                              <?php endif ?>
+                              <label for="exampleInputPassword1">Password</label>
+                              <input type="password" class="form-control shadow-sm password" id="exampleInputPassword1" name="password" value="<?= set_value('password'); ?>">
+                              <i class="fas fa-eye-slash Show"></i>
+                              <?= form_error('password', '<small id="emailHelp" class="form-text text-muted">', '</small>'); ?>
+                              </div>
+
+                              <?php if (form_error('password2')) : ?>
+                                 <div class="form-group error">
+                                 <?php else : ?>
+                                    <div class="form-group">
+                                    <?php endif ?>
+                                    <label for="exampleInputPassword2">Repeat Password</label>
+                                    <input type="password" class="form-control shadow-sm password2" id="exampleInputPassword2" name="password2" value="<?= set_value('password2'); ?>">
+                                    <?= form_error('password2', '<small id="emailHelp" class="form-text text-muted">', '</small>'); ?>
+                                    <i class="fas fa-exclamation-circle iconError"></i>
+                                    </div>
+
+                                    <div class="form-group">
+                                       <button type="submit" class="btn btn-danger btn-block shadow-sm">Create Account!</button>
+                                    </div>
          </form>
       </div>
    </div>
 </div>
+<!-- 
+<style>
+   body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 100vh;
+   }
+
+   .Email {
+      text-align: center;
+      margin: auto;
+      border: 1px solid black;
+      font-weight: normal;
+      padding: 30px;
+      border-radius: 4px;
+      box-shadow: 0 0 5px rgba(0, 0, 0, .2);
+   }
+
+   .header h3 {
+      font-family: 'Anton';
+      text-transform: uppercase;
+      font-size: 35px;
+      margin-top: -10px;
+      margin-bottom: 5px;
+   }
+
+   h6 {
+      margin-bottom: 20px;
+   }
+
+   p {
+      margin-bottom: 0px;
+   }
+
+   .content {
+      color: grey;
+      margin-bottom: 15px;
+      font-weight: bold;
+   }
+
+   a {
+      width: 100%;
+      color: white;
+      background: #1641ff;
+      border-radius: 20px;
+      padding: 8px 50px;
+      text-decoration: none !important;
+      font-weight: bold;
+      box-shadow: 0 15px 10px -15px #1641ff;
+      transition: .3s linear;
+   }
+
+   
+   a:hover {
+      box-shadow: 0 15px 10px -12px #1641ff;
+      color: white;
+   }
+</style>
+<div class="Email">
+   <div class="header">
+      <h3>Bikes</h3>
+      <h6>Welcome to Bikes Shop!</h6>
+   </div>
+
+   <div class="content">
+      <p>Dear : muhammadfatah398@gmail</p>
+      <span>Click active for verification Account!</span> <br>
+   </div>
+   <div class="btn">
+      <a href="">Active</a>
+   </div>
+</div> -->
